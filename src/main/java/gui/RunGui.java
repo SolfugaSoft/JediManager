7f356490923027cf5229b965cbed43f9f32e0c7d
SolfugaSoft <solfugasoft@gmail.com>
2020-03-11 22:43:00 +0100

    add textfilds and jComoBox

---
+++ src/main/java/gui/RunGui.java
@@ -0,0 +1,14 @@
+package gui;
+
+import java.awt.*;
+
+public class StartFrame {
+
+    public StartFrame() {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                
+            }
+        });
+    }
+}
