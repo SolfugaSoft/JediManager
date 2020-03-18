83a7a6ffeb78e4ed868134a3eb3dc0cd46d70c78
SolfugaSoft <solfugasoft@gmail.com>
2020-03-18 22:40:57 +0100

    add method  with test data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         RunGui Gui = new RunGui();
         JDBCConnector jdbcConnector = new JDBCConnector();
+        jdbcConnector.add();
 
 
     }
--- src/main/java/gui/JDBCConnector.java
+++ src/main/java/gui/JDBCConnector.java
@@ -1,16 +1,17 @@
 package gui;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import orderAndKnights.Functions;
 
-public class JDBCConnector {
+import java.io.File;
+import java.sql.*;
+
+public class JDBCConnector implements Functions {
+
 
     //    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String URL = "jdbc:mysql://remotemysql.com:3306/nX6SWFrxIM";
-    static final String USER = "nX6SWFrxIM";
-    static final String PASSWORD = "qewt0kkgLE";
+    static final String URL = "jdbc:mysql://remotemysql.com:3306/qeabw7Rsms";
+    static final String USER = "qeabw7Rsms";
+    static final String PASSWORD = "v5HGLnVJDe";
 
 
     public Connection StartConnection() {
@@ -26,8 +27,29 @@ public class JDBCConnector {
             System.out.println("Wygibało dane do połączenia");
             e.printStackTrace();
         }
-        return connection;
 
+        return connection;
     }
 
+    //    public void add(String name, String swordColor,int power, String forceSite)
+    public void add() {
+        String tableName = "JediKnight";
+        String addQuery = "INSERT INTO " + tableName + "(FirstName,SwordColor,Power,ForceSite) VALUES";
+
+        try {
+            Statement statement = StartConnection().createStatement();
+            statement.executeUpdate(
+                    "INSERT INTO JediKnight(FirstName,SwordColor,Power,ForceSite) VALUES (\n" +
+                            "'Anna', 'Oliwkowy', 666, 'GUnit');"
+            );
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("poszło");
+    }
+
+    public void get() {
+
+    }
 }
